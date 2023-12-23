ba4390df843d4a3e0fbc885cd867cec5c69dd4fc
tiago-rodrigues1 <tiagords127@gmail.com>
2023-12-23 00:04:10 -0300

    feat: create rankink function

--- app/Models/Partida.php
+++ app/Models/Partida.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Partida extends Model
 {
@@ -18,11 +19,17 @@ class Partida extends Model
     public static function create($data) {
         try {
             $partida = new Partida($data);
-            $partida->player=Auth()::id();
+            $partida->player()->associate(Auth::user());
             
             return $partida->save();
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             return null;
         }
+    }
+
+    public static function listar() {
+        $partidas = Partida::orderby('acertos', 'desc')->orderBy('erros', 'asc')->orderBy('created_at', 'desc')->get();
+
+        return $partidas;
     }
 }
